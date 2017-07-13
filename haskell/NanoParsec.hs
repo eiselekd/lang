4b9218a06c1696a3efa8fefe04db887c93e1f448
Konrad Eisele <eiselekd@gmail.com>
2017-07-13 23:12:00 +0200

    all

---
+++ haskell/NanoParsec.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+
+{- http://dev.stephendiehl.com/fun/002_parsers.html -}
+
+module NanoParsec where
+
+import Data.Char
+import Control.Monad
+import Control.Applicative
+
+newtype Parser a = Parser { parse :: String -> [(a,String)] }
+
+runParser :: Parser a -> String -> a
+runParser m s =
+  case parse m s of
+    [(res, [])] -> res
+    [(_, rs)]   -> error "Parser did not consume entire stream."
+    _           -> error "Parser error."
