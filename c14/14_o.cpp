2ad9e85a881beac01526541f0e3b119401b4dd09
Konrad Eisele <eiselekd@gmail.com>
2017-09-23 16:32:39 +0000

    all

--- c14/14_o.cpp
+++ c14/14_o.cpp
@@ -3,14 +3,39 @@
 #include <string>
 #include <vector>
 
-class b {
+class a {
 public:
-  b(std::initializer_list<int> a) {
+  a(std::initializer_list<int> a) {
   }
 };
 
-int main(int argc, char **argv) {
-  b x({1,2,3,4});
+class c {
+public:
+  int e0 = 2;
+  int e1 = 3;
+};
 
+class b {
+public:
+  b() : b0{0,1} {
+  }
+  b(std::initializer_list<int> l) : b0{*l.begin(),2}{
+  }
+  struct c b0;
+  int d0 = 1;
+  int d1 = 2;
+};
+
+class d {
+public:
+  d(int a, int b) {};
+  int e0 = 2;
+  int e1 = 3;
+};
+
+int main(int argc, char **argv) {
+  a x({1,2,3,4});
+  b y{1,2};
+  d z{1,2};
   return 0;
 }
