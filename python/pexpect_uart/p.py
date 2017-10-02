5b56c8362dcbe693e5f6d9ee408476b8b0fd557b
Konrad Eisele <konrad.eisele@qamcom.se>
2017-10-02 20:44:34 +0200

    all

--- python/pexpect_uart/p.py
+++ python/pexpect_uart/p.py
@@ -1,32 +1,31 @@
-import sys, os
+import sys, os, time
 from serial import *;
 from pexpect import *; 
 import pexpect.fdpexpect as fdpexpect;
 
-ser = Serial("/dev/ttyUSB0",115200);
+ser = Serial("/dev/pts/21",115200);
 reader = fdpexpect.fdspawn(ser);
 reader.logfile = sys.stdout
 
 reader.send("\n");
-state = reader.expect(["root@Carambola2", TIMEOUT], timeout=1)
+state = reader.expect(["root@.*", TIMEOUT], timeout=1)
 if (state == 1):
     print ("not powered on, please reset");
     try:
-        state = reader.expect(["ieee80211 phy0", "Registered protocol family", TIMEOUT], timeout=30)
+        state = reader.expect(["Please press Enter to activate this console"])
         if (state == 0):
-            print ("start");
+            print ("++++++ Please press Enter to activate this console");
         elif (state == 1):
             print ("proto");
     except TIMEOUT:
         print("Got timeout")
         exit(1)
 
-print("Ready for pexpect");
-
 reader.send("\n");
-reader.expect(["root@Carambola2.*", TIMEOUT], timeout=1)
+reader.expect(["root@.*"])
+
 reader.send("netstat -n -t\n");
-reader.expect(["root@Carambola2.*", TIMEOUT], timeout=1)
+reader.expect(["root@.*"])
 
 print("netstat on target:");
 print(reader.before)
