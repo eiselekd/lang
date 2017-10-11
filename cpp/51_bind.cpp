b80dd606822e49a4b344bd0af7ff04f25c42d63b
Konrad Eisele <eiselekd@gmail.com>
2017-10-11 12:11:53 +0000

    all

--- cpp/51_bind.cpp
+++ cpp/51_bind.cpp
@@ -15,7 +15,7 @@ public:
 
   class iterator {
   public:
-    mypair & operator *()  { return i_; }
+    const mypair & operator *() const { return i_; }
     const iterator &operator ++() { ++i_.first; return *this; }
     iterator operator ++(int) { iterator copy(*this); ++i_.first; return copy; }
 
@@ -31,6 +31,28 @@ public:
   iterator end() const { return iterator(10); }
 };
 
+
+
+/// Partial specialization for std::pair
+template<>
+struct std::__is_tuple_like_impl<mypair> : true_type
+{ };
+
+/// Partial specialization for std::pair
+template<>
+struct std::tuple_size<mypair>
+  : public std::integral_constant<std::size_t, 2> { };
+
+/// Partial specialization for std::pair
+template<>
+struct std::tuple_element<0, mypair>
+{ typedef int type; };
+
+/// Partial specialization for std::pair
+template<>
+struct std::tuple_element<1, mypair>
+{ typedef int type; };
+
 template<std::size_t _Int>
 struct __mypair_get;
 
@@ -40,6 +62,9 @@ struct __mypair_get<0>
   static constexpr int&
   __get(mypair& __pair) noexcept
   { return __pair.first; }
+  static constexpr const int&
+  __get_const(const mypair& __pair) noexcept
+  { return __pair.first; }
 };
 
 template<>
@@ -48,8 +73,10 @@ struct __mypair_get<1>
   static constexpr int&
   __get(mypair& __pair) noexcept
   { return __pair.second; }
+  static constexpr const int&
+  __get_const(const mypair& __pair) noexcept
+  { return __pair.second; }
 };
-
 
 // use std::tuple_element to generate first,second accessors
 // structual binding will reference
@@ -60,9 +87,17 @@ constexpr typename std::tuple_element<_Int, mypair>::type&
 get(mypair& __in) noexcept
 { return __mypair_get<_Int>::__get(__in); }
 
+template<std::size_t _Int>
+constexpr const typename std::tuple_element<_Int, mypair>::type&
+get(const mypair& __in) noexcept
+{ return __mypair_get<_Int>::__get_const(__in); }
+
+
+
+
 int main(int argc, char **argv) {
   a v0;
-  for (auto & [i,j] : v0) {
+  for (const auto & [i,j] : v0) {
     std::cout << i << "," << j << std::endl;
   }
   return 0;
