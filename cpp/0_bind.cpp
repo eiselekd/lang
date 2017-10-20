c776404f207a59a5b46070ac7c6f1d524fd9878a
Konrad Eisele <eiselekd@gmail.com>
2017-10-20 08:34:13 +0000

    all

--- cpp/0_bind.cpp
+++ cpp/0_bind.cpp
@@ -1,13 +1,52 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 #include <functional>
 using namespace std;
 using namespace std::placeholders;
 
+/*
+
+* test
+** NEXT [#B] test
+
+* dada
+  |------+------+----|
+  | re   | sdsd | sd |
+  | sdsd | sd   |    |
+
+
+#+BEGIN_SRC C++
+
+int func1() {
+  int c = 0;
+  return 10;
+}
+
+#+END_SRC
+
+
+
+#+RESULTS:
+[[file:test3.png]]
+
+
+* #
+
+*/
+
 int add(int a, int b) {
+
   return a+b;
 }
 
+/*
+next
+
+* Next chapter
+** new functions
+* #
+*/
 int main(int argc, char **argv) {
   function<int (int) > add4 = bind(&add, 4, _1);
   cout << add4(5) << endl;
