feaf7f401191a68d877c1a4a4e30fd95871ee9a6
Konrad Eisele <eiselekd@gmail.com>
2017-12-25 17:55:10 +0000

    all

--- perl/tie.pl
+++ perl/tie.pl
@@ -7,3 +7,51 @@ sub STORE {
 }
 1;
 
+
+package Tie::Folded;
+use strict;
+use Tie::Hash;
+use vars qw(@ISA);
+@ISA = qw(Tie::StdHash);
+sub STORE {
+    my ($self, $key, $value) = @_;
+    return $self->{lc $key} = $value;
+    } 
+sub FETCH {
+    my ($self, $key) = @_;
+    return $self->{lc $key};
+} 
+sub EXISTS {
+    my ($self, $key) = @_;
+    return exists $self->{lc $key};
+} 
+sub DEFINED {
+    my ($self, $key) = @_;
+    return defined $self->{lc $key};
+} 
+1;
+
+
+tie %h, 'Foo', 'a' => 1
+$obj = Foo-->TIEHASH('a',1);
+$h{a}
+$obj-->FETCH ('a')
+$h{a} = 1
+$obj-->STORE ('a', 1)
+delete $h{a}
+$obj-->DELETE('a')
+exists $h{a}
+$obj-->EXISTS('a')
+keys (%h),values(%h)
+each (%h)
+$lk = $obj-->FIRSTKEY ();
+do {
+   $val = $obj-->FETCH{$lk};
+} while ($lk = $obj-->NEXTKEY($lk));
+%h = ()
+$obj-->CLEAR()
+%h = (a=> 1)
+$obj-->CLEAR()
+$obj-->STORE('a',1)
+untie %h
+$obj-->DESTROY()
