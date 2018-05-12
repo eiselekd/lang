ae19e6530fb7b9508b08c95b3bd9df8f3637e8b7
Konrad Eisele <eiselekd@gmail.com>
2018-05-12 11:24:37 +0200

    all

---
+++ rev/miasm/tigress.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#http://www.miasm.re/blog/2017/10/05/playing_with_dynamic_symbolic_execution.html
+
+from miasm2.analysis.sandbox import Sandbox_Linux_x86_64
+from miasm2.jitter.csts import PAGE_READ
+
+def xxx_strtoul(jitter):
+    ret_ad, args = jitter.func_args_systemv(["nptr", "endptr", "base"])
+    assert args.endptr == 0
+    content = jitter.get_str_ansi(args.nptr)
+    value = int(content, args.base)
+    print "%r -> %d" % (content, value)
+    jitter.func_ret_systemv(ret_ad, value)
+    
+
+# Create sandbox
+parser = Sandbox_Linux_x86_64.parser(description="ELF sandboxer")
+parser.add_argument("filename", help="ELF Filename")
+options = parser.parse_args()
+options.jitter = "llvm"
+
+
+# Force environment simulation
+options.mimic_env = True
+# Dummy argument: 123456789
+options.command_line = ["".join(chr(0x30 + i) for i in xrange(1, 10))]
+
+# Instantiate and run
+sb = Sandbox_Linux_x86_64(options.filename, options, globals())
+
+# Init stack canary
+sb.jitter.ir_arch.do_all_segm = True
+FS_0_ADDR = 0x7ff70000
+sb.jitter.cpu.FS = 0x4
+sb.jitter.cpu.set_segm_base(sb.jitter.cpu.FS, FS_0_ADDR)
+sb.jitter.vm.add_memory_page(
+    FS_0_ADDR + 0x28, PAGE_READ, "\x42\x42\x42\x42\x42\x42\x42\x42", "Stack canary FS[0x28]")
+
+
+sb.run()
+
