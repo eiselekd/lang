0cde61cd3f6f6e4ec10f7b5017fc3bc39ba39131
Konrad Eisele <konrad.eisele@qamcom.se>
2018-10-06 20:44:13 +0200

    all

---
+++ cpp/101_stack.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+class aclass {
+public:
+    aclass(int size) : size_(size) {};
+    int size_;
+    char data[0];
+};
+
+void f(int size)
+{
+    char v[sizeof(aclass) + size];
+    new(static_cast<void*>(&v)) aclass(size);
+}
+
+int main(int argc, char **argv)
+{
+    /*
+      placement-new:
+       http://www.scs.stanford.edu/~dm/home/papers/c++-new.html
+       https://stackoverflow.com/questions/222557/what-uses-are-there-for-placement-new
+    */
+
+    char *buf  = new char[sizeof(std::string)]; // pre-allocated buffer
+    std::string *p = new (buf) std::string("hi");    // placement new
+    std::string *q = new std::string("hi");
+
+    f(10);
+    f(100);
+    f(1000);
+    return 0;
+}
---
+++ cpp/102_stack.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+
+/*
+ * https://stackoverflow.com/questions/52681624/destructor-on-placement-new?noredirect=1#comment92291870_52681624
+ */
+template <typename T>
+class stack_placement {
+private:
+    T *m_ptr;
+public:
+    stack_placement(void *area, int size) {
+	m_ptr = new(area) T(size);
+    }
+    stack_placement(const stack_placement &) = delete;
+    ~stack_placement() {
+	m_ptr->~T();
+    }
+    void operator=(const stack_placement &) = delete;
+    T *operator->() {
+	return m_ptr;
+    }
+};
+
+class aclass {
+public:
+    aclass(int size) : size_(size) { printf("aclass\n"); };
+    ~aclass() {}
+    int size_;
+    char data[0];
+};
+
+void f(int size)
+{
+    intptr_t v0[(sizeof(aclass) + size + sizeof(intptr_t) - 1)/sizeof(intptr_t)];
+    intptr_t v1[(sizeof(aclass) + size + sizeof(intptr_t) - 1)/sizeof(intptr_t)];
+    aclass *p = new(static_cast<void*>(&v0)) aclass(size);
+    p->~aclass();
+
+    stack_placement<aclass> v(v1, size);
+
+}
+
+int main(int argc, char **argv)
+{
+    /*
+      placement-new:
+       http://www.scs.stanford.edu/~dm/home/papers/c++-new.html
+       https://stackoverflow.com/questions/222557/what-uses-are-there-for-placement-new
+    */
+    f(10);
+    f(100);
+    return 0;
+}
