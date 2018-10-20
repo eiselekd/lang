c2060e81ad78b12c07289523fd15262a1f6440cd
Konrad Eisele <eiselekd@gmail.com>
2018-10-20 15:35:42 +0200

    all

--- sh/expr.sh
+++ sh/expr.sh
@@ -5,3 +5,8 @@ if ! [[ $a =~ "[test] -f  18" ]]; then
     b=$(expr "$a" : '\[[a-z]*\].*\([0-9][0-9][0-9].*\)')
     echo "[test] -f $b"
 fi
+
+a="[test] testall -f out.txt"
+if ! [[ $a =~ "\[([a-z]+)\].*\-d ([a-z\.]+)" ]]; then
+    echo "Found : '$a'"
+fi
