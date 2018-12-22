5e9b2946e915d6ef7626b3cf96524a7fc66b89bf
Konrad Eisele <eiselekd@gmail.com>
2018-12-22 22:24:30 +0100

    all

---
+++ python/decorator/a.py
@@ -0,0 +1,24 @@
+# decorator with arguments returns a decorator that returns
+# decorated function
+def noaction(func):
+    def gen(*args,**kwargs):
+        return func("x",*args)
+    return gen
+
+# decorator with arguments returns a decorator that returns
+# decorated function
+def action(arg0):
+    def decor(functowrap):
+        def gen(*args,**kwargs):
+            return functowrap(arg0, *args)
+        return gen
+    return decor
+
+@action("a0") # there is a call to action() that will create the decorator
+@noaction
+def m(*args):
+    print(str(args))
+
+m("m0")
+m("m0","m1")
+
