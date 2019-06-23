a30c5aa4450ae2e2fc4ccdcd9ae5cdddbdb3427e
Konrad Eisele <eiselekd@gmail.com>
2019-06-23 18:06:34 +0200

    all

--- cpp/401_tagged_union.cpp
+++ cpp/401_tagged_union.cpp
@@ -35,5 +35,16 @@ int main(int argc, char** argv) {
 		 (Float, std::cout << "Float" << e.v << std::endl;)
 	    );
     }
+
+// Macro to obtain a numbered macro for argument counts
+// - Raw variant
+#define TU_GM_I(SUF,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,COUNT,...) SUF##COUNT
+#define TU_GM(SUF,...) TU_EXP1( TU_GM_I(SUF, __VA_ARGS__,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0) )
+
+#define ARGS(...)  TU_GM(,__VA_ARGS__)
+
+    std::cout << "Count macro args:" << ARGS(a1,a2,a3) << std::endl;
+    std::cout << "Count macro args:" << ARGS(a1) << std::endl;
+
     return 0;
 }
