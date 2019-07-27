be8eaf7b11afb852b062aed02e7f2dfd4354c4a1
Konrad Eisele <eiselekd@gmail.com>
2019-07-27 16:51:02 +0200

    all

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -1,6 +1,32 @@
+open Lex;;
+open Token;;
 
-let _ =
-  (Printf.fprintf stderr "Test\n"; exit 1)
+let add a = a + 1;;
+
+let introtext = Printf.fprintf stdout "Lex '%s'" Sys.argv.(1);;
+
+let nexttok lexbuf =
+  let tok = Lex.token lexbuf in
+  if tok = EOF
+  then false
+  else
+    (Printf.fprintf stdout "'%s'" (Token.string_of_tok tok); true)
+;;
+
+let doscan fn =
+  let fname = fn in
+  let lexbuf = Lexing.from_channel (open_in fname) in
+  let spos = { lexbuf.Lexing.lex_start_p with Lexing.pos_fname = fname } in
+  let cpos = { lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = fname } in
+  lexbuf.Lexing.lex_start_p <- spos;
+  lexbuf.Lexing.lex_curr_p <- cpos;
+  while (nexttok lexbuf) do
+    Printf.fprintf stdout ".";
+  done
+;;
+
+let _ = (introtext;
+         doscan Sys.argv.(1) )
 ;;
 
 (*
--- ocaml/token.ml
+++ ocaml/token.ml
@@ -1,11 +1,17 @@
 type token =
-
-    (* Expression operator symbols *)
     PLUS
   | MINUS
   | STAR
   | SLASH
 
   | EOF
+;;
 
+let rec string_of_tok t =
+  match t with
+      PLUS       -> "+"
+    | MINUS      -> "-"
+    | STAR       -> "*"
+    | SLASH      -> "/"
+    | EOF        -> "<EOF>"
 ;;
