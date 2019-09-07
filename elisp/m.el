38ac6adf35c42a46d867447d4590c96766f3d1ea
Konrad Eisele <eiselekd@gmail.com>
2019-09-07 22:27:25 +0200

    all

--- elisp/m.el
+++ elisp/m.el
@@ -1,3 +1,6 @@
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Backquote.html#Backquote
+;; ,@ : splice list
+
 (defmacro addpart (&rest args)
   (let ((s* (and (symbolp (car args))
                  (pop args))))
@@ -5,7 +8,7 @@
        ,@(if s*
              `((let ((,s* ,s))
 		 ,@(cdr args)))
-	   (cdr(cdr args)))
+	   (cdr (cdr args)))
        )))
 
 (defun f0 ()
