18324715e67a5ed5968bef1b96a8d18b677d325f
Konrad Eisele <eiselekd@gmail.com>
2019-11-16 17:40:56 +0100

    all

--- sh/scan.sh
+++ sh/scan.sh
@@ -1,16 +1,25 @@
-local -A donges
+declare -A mac
+declare -a dongles
 
 IFS=$'\n'
-hciline="^hci([0-9]):"
+hciline="^(hci[0-9]+):"
 bdadd="BD Address: ([0-9A-F:]+)"
+c=""
 for i in $(hciconfig); do
     if [[ $i =~ $hciline ]]
     then
-	n="${BASH_REMATCH[1]}"
-	echo $n;
+	c="${BASH_REMATCH[1]}"
+	dongels+=$c
     elif [[ $i =~ $bdadd ]]
     then
-	n="${BASH_REMATCH[1]}"
-	echo $n;
+	mac[$c]="${BASH_REMATCH[1]}"
     fi
+done
+
+for m in ${!dongels[@]}; do
+    echo $m ${dongels[$m]}
+done
+
+for m in ${!mac[@]}; do
+    echo $m ${mac[$m]}
 done
